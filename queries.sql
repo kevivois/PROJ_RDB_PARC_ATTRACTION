c43335b707f42b3287c6c52c75237bb4387e7c4a
Gom01 <96956562+Gom01@users.noreply.github.com>
2024-12-18 16:54:10 +0100

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,26 +1,30 @@
---Quelles attractions puis-je faire si j'ai 14 ans et un billet VIP Entry ?
-SELECT r.name, r.minimum_age FROM ticketTypes AS t
+-- Quelles attractions puis-je faire si j'ai 14 ans et un billet VIP Entry ?
+SELECT r.name, r.minimum_age 
+FROM ticketTypes AS t
 JOIN visitors_rollerCoasters AS v
 ON t.id = v.ticket_id
 JOIN rollercoasters AS r
 ON r.id = v.id
 WHERE r.minimum_age <= 14 AND t.name LIKE 'VIP Entry';
 
---Quelles est la moyenne des avis ?
-SELECT round((AVG(f.rating)),2) AS "Average ratings" FROM feedbacks AS f
+-- Quelle est la moyenne des avis ?
+SELECT ROUND((AVG(f.rating)), 2) AS "Moyenne des avis" 
+FROM feedbacks AS f
 JOIN rollercoasters AS r
 ON f.rollercoaster_id = r.id;
 
---Quelle est la pire attraction (en moyenne) ?
-SELECT r.name, round(AVG(f.rating),2) AS "Average ratings" FROM feedbacks AS f
+-- Quelle est la pire attraction (en moyenne) ?
+SELECT r.name, ROUND(AVG(f.rating), 2) AS "Moyenne des avis" 
+FROM feedbacks AS f
 JOIN rollercoasters AS r
 ON f.rollercoaster_id = r.id
 GROUP BY r.id
-ORDER BY "Average ratings" ASC
+ORDER BY "Moyenne des avis" ASC
 LIMIT 1;
 
---Est-ce que le Park Manager à déjà travaillé à la fois dans un shop (sales_visitors) et dans une attractions.
-SELECT e.firstname, e.lastname FROM sales_visitors AS s
+-- Est-ce que le Park Manager a déjà travaillé à la fois dans un shop (sales_visitors) et dans une attraction ?
+SELECT e.firstname, e.lastname 
+FROM sales_visitors AS s
 JOIN employees AS e
 ON s.employee_id = e.id
 JOIN employees_rollercoasters AS er
@@ -29,35 +33,31 @@ JOIN roles AS r
 ON r.id = e.role_id
 WHERE r.role LIKE 'Park Manager';
 
---Quelles visiteurs ont achetés aux moins 2 objets.
+-- Quels visiteurs ont acheté au moins 2 objets ?
+SELECT visitors.id, visitors.firstname || ' ' || visitors.lastname AS visitor, COUNT(sales_visitors.id) 
+FROM visitors
+INNER JOIN sales_visitors 
+ON sales_visitors.visitor_id = visitors.id
+GROUP BY visitors.id
+HAVING COUNT(sales_visitors.id) >= 2;
 
-select visitors.id,visitors.firstname || ' ' || visitors.lastname as visitor,count(sales_visitors.id) from visitors
-inner join sales_visitors on sales_visitors.visitor_id = visitors.id
-group by visitors.id
-having count(sales_visitors.id) >= 2;
-
-
---Combien de produits ont été acheté le 12 décembre ?
-SELECT COUNT(s.product_id) FROM sales_visitors AS s
+-- Combien de produits ont été achetés le 12 décembre ?
+SELECT COUNT(s.product_id) 
+FROM sales_visitors AS s
 GROUP BY s.date
-HAVING s.date = '2024-12-01'
+HAVING s.date = '2024-12-12'
 ORDER BY COUNT(s.product_id) DESC;
 
-
---Quels clients ont acheté quels objects a quel employé et quand 
-
-select visitors.firstname || ' ' || visitors.lastname as visitor,employees.firstname || ' ' || employees.lastname as employee,products.product_name as product,products.quantity*products.price as "total price",sales_visitors.date as date from products
-inner join sales_visitors on sales_visitors.product_id = products.id
-inner join visitors on visitors.id = sales_visitors.visitor_id
-inner join employees on employees.id = sales_visitors.id
-
-
-
-
-
-
-
-
-
-
-
+-- Quels clients ont acheté quels objets, à quel employé et quand ?
+SELECT visitors.firstname || ' ' || visitors.lastname AS client, 
+       employees.firstname || ' ' || employees.lastname AS employé, 
+       products.product_name AS produit, 
+       products.quantity * products.price AS "Prix total", 
+       sales_visitors.date AS date 
+FROM products
+INNER JOIN sales_visitors 
+ON sales_visitors.product_id = products.id
+INNER JOIN visitors 
+ON visitors.id = sales_visitors.visitor_id
+INNER JOIN employees 
+ON employees.id = sales_visitors.employee_id;
