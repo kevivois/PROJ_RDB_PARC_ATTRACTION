d86c8b362c2e4ea859fe3f46b7038d751d5832ac
Gom01 <96956562+Gom01@users.noreply.github.com>
2024-12-18 13:53:32 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,102 +1,90 @@
-CREATE TABLE role (
-    id SERIAL PRIMARY KEY,
-    role VARCHAR(50) NOT NULL
-);
-
-CREATE TABLE ticket_type (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    price INT NOT NULL CHECK (price > 0)
-);
-
-CREATE TABLE rollerCoaster (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(150) NOT NULL,
-    opening TIME,
-    closing TIME,
-    minimum_age INT NOT NULL CHECK (minimum_age > 0),
-    ticket_required_id INT NOT NULL,
-    CONSTRAINT fk_roller_ticket FOREIGN KEY (ticket_required_id) REFERENCES ticket_type (id)
-);
-
 CREATE TABLE visitors (
     id SERIAL PRIMARY KEY,
-    firstname VARCHAR(100) NOT NULL,
-    lastname VARCHAR(100) NOT NULL,
+    firstname VARCHAR NOT NULL,
+    lastname VARCHAR NOT NULL,
     age INT NOT NULL CHECK (age > 0)
 );
 
-CREATE TABLE feedback (
+CREATE TABLE feedbacks (
     id SERIAL PRIMARY KEY,
     rollerCoaster_id INT NOT NULL,
     visitor_id INT NOT NULL,
     date DATE NOT NULL,
-    comments TEXT,
-    ratings INT NOT NULL CHECK (ratings >= 0 AND ratings <= 5),
-    CONSTRAINT fk_feedback_rollerCoaster FOREIGN KEY (rollerCoaster_id) REFERENCES rollerCoaster (id),
-    CONSTRAINT fk_feedback_visitor FOREIGN KEY (visitor_id) REFERENCES visitors (id)
+    comment TEXT,
+    rating INT NOT NULL CHECK (rating >= 0 AND rating <= 5),
+    CONSTRAINT fk_feedbacks_rollerCoaster FOREIGN KEY (rollerCoaster_id) REFERENCES rollerCoasters (id),
+    CONSTRAINT fk_feedbacks_visitor FOREIGN KEY (visitor_id) REFERENCES visitors (id)
 );
 
 CREATE TABLE products (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    price INT NOT NULL CHECK (price > 0)
-);
-
-CREATE TABLE shops (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(20) NOT NULL,
-    id_products INT,
-    CONSTRAINT fk_shops_products FOREIGN KEY (id_products) REFERENCES products (id)
+    product_name VARCHAR NOT NULL,
+    price DECIMAL NOT NULL CHECK (price > 0)
 );
 
 CREATE TABLE sales_visitors (
     id SERIAL PRIMARY KEY,
-    id_products INT NOT NULL,
-    id_visitors INT NOT NULL,
-    CONSTRAINT fk_sales_productss FOREIGN KEY (id_products) REFERENCES products (id),
-    CONSTRAINT fk_sales_visitors FOREIGN KEY (id_visitors) REFERENCES visitors (id)
+    date DATE NOT NULL,
+    quantity INT NOT NULL CHECK (quantity > 0),
+    product_id INT NOT NULL,
+    visitor_id INT NOT NULL,
+    CONSTRAINT fk_sales_visitors_product FOREIGN KEY (product_id) REFERENCES products (id),
+    CONSTRAINT fk_sales_visitors_visitor FOREIGN KEY (visitor_id) REFERENCES visitors (id)
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
     id SERIAL PRIMARY KEY,
     role_id INT NOT NULL,
-    firstname VARCHAR(100) NOT NULL,
-    lastname VARCHAR(100) NOT NULL,
-    salary INT CHECK (salary > 0),
-    CONSTRAINT fk_employee_role FOREIGN KEY (role_id) REFERENCES role (id)
+    firstname VARCHAR NOT NULL,
+    lastname VARCHAR NOT NULL,
+    salary DECIMAL NOT NULL CHECK (salary > 0),
+    CONSTRAINT fk_employees_role FOREIGN KEY (role_id) REFERENCES roles (id)
 );
 
-CREATE TABLE visitor_tickets (
+CREATE TABLE roles (
+    id SERIAL PRIMARY KEY,
+    role VARCHAR NOT NULL
+);
+
+CREATE TABLE ticketTypes (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    price DECIMAL NOT NULL CHECK (price > 0)
+);
+
+CREATE TABLE rollerCoasters (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    opening TIME NOT NULL,
+    closing TIME NOT NULL,
+    minimum_age INT NOT NULL CHECK (minimum_age > 0),
+    ticket_required_id INT NOT NULL,
+    CONSTRAINT chk_opening_closing CHECK (opening < closing),
+    CONSTRAINT fk_rollerCoasters_ticketType FOREIGN KEY (ticket_required_id) REFERENCES ticketTypes (id)
+);
+
+CREATE TABLE visitors_tickets (
     id SERIAL PRIMARY KEY,
     visitor_id INT NOT NULL,
     ticket_id INT NOT NULL,
     date DATE NOT NULL,
-    CONSTRAINT fk_visitor_tickets_visitor FOREIGN KEY (visitor_id) REFERENCES visitors (id),
-    CONSTRAINT fk_visitor_tickets_ticket FOREIGN KEY (ticket_id) REFERENCES ticket_type (id)
+    CONSTRAINT fk_visitors_tickets_visitor FOREIGN KEY (visitor_id) REFERENCES visitors (id),
+    CONSTRAINT fk_visitors_tickets_ticket FOREIGN KEY (ticket_id) REFERENCES ticketTypes (id)
 );
 
-CREATE TABLE employee_shops (
-    id SERIAL PRIMARY KEY,
-    employee_id INT NOT NULL,
-    shops_id INT NOT NULL,
-    CONSTRAINT fk_employee_shops_employee FOREIGN KEY (employee_id) REFERENCES employee (id),
-    CONSTRAINT fk_employee_shops_shops FOREIGN KEY (shops_id) REFERENCES shops (id)
-);
-
-CREATE TABLE rollerCoaster_ticket (
+CREATE TABLE visitors_rollerCoasters (
     id SERIAL PRIMARY KEY,
     rollerCoaster_id INT NOT NULL,
     ticket_id INT NOT NULL,
-    CONSTRAINT fk_visitor_rc_roller FOREIGN KEY (rollerCoaster_id) REFERENCES rollerCoaster (id),
-    CONSTRAINT fk_visitor_rc_billet FOREIGN KEY (ticket_id) REFERENCES visitors (id)
+    CONSTRAINT fk_visitors_rollerCoasters_rollerCoaster FOREIGN KEY (rollerCoaster_id) REFERENCES rollerCoasters (id),
+    CONSTRAINT fk_visitors_rollerCoasters_ticket FOREIGN KEY (ticket_id) REFERENCES ticketTypes (id)
 );
 
-CREATE TABLE employee_rollerCoaster (
+CREATE TABLE employees_rollerCoasters (
     id SERIAL PRIMARY KEY,
     date DATE NOT NULL,
     rollerCoaster_id INT NOT NULL,
     employee_id INT NOT NULL,
-    CONSTRAINT fk_employee_rc_roller FOREIGN KEY (rollerCoaster_id) REFERENCES rollerCoaster (id),
-    CONSTRAINT fk_employee_rc_employee FOREIGN KEY (employee_id) REFERENCES employee (id)
+    CONSTRAINT fk_employees_rollerCoasters_rollerCoaster FOREIGN KEY (rollerCoaster_id) REFERENCES rollerCoasters (id),
+    CONSTRAINT fk_employees_rollerCoasters_employee FOREIGN KEY (employee_id) REFERENCES employees (id)
 );
