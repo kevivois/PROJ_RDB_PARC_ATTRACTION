898901645ddce1e4f8266020ecc36e06a1f38240
kevivois <103002352+kevivois@users.noreply.github.com>
2024-12-18 15:46:14 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -80,6 +80,7 @@ CREATE TABLE visitors_rollerCoasters (
     id SERIAL PRIMARY KEY,
     rollerCoaster_id INT NOT NULL,
     ticket_id INT NOT NULL,
+    date date not null
     CONSTRAINT fk_visitors_rollerCoasters_rollerCoaster FOREIGN KEY (rollerCoaster_id) REFERENCES rollerCoasters (id),
     CONSTRAINT fk_visitors_rollerCoasters_ticket FOREIGN KEY (ticket_id) REFERENCES ticketTypes (id)
 );
